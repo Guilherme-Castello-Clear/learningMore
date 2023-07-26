2a13f1842fb03803e630d31ed0ceab8fc286cc27
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-07-26 16:16:08 -0300

    Feat: Completed Challange

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, FlatList, Switch } from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, FlatList, Switch, TextInput } from 'react-native'
 
 import {Picker} from '@react-native-picker/picker'
 import Pessoas from './src/Pessoas/'
@@ -9,16 +9,56 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      status: false
+      nome: '',
+      idade: 1,
+      sexo: 1,
+      limite: 100,
+      estudante: false,
     }
+    this.enviar = this.enviar.bind(this)
+  }
+
+  enviar(){
+    alert(`
+      Nome: ${this.state.nome}
+      Idade: ${this.state.idade}
+      Sexo: ${this.state.sexo == 1 ? 'Masculino' : 'Feminino'}
+      Limite: ${this.state.limite.toFixed()}
+      Estudante? ${this.state.estudante ? 'Sim' : 'Não'}
+    `)
   }
 
   render(){
 
     return(
       <View style={styles.container}>
-        <Switch value={this.state.status} thumbColor={'red'} onValueChange={e => this.setState({status: e})}/>
-        <Text>{this.state.status ? "Ativo" : "Inativo"}</Text>
+        <View style={styles.label}>
+          <Text style={styles.titulo}>Insira seu nome:</Text>
+          <TextInput style={styles.input} onChangeText={(evt) => this.setState({nome: evt})} label={'A'}/>
+        </View>
+        <View style={styles.label}>
+          <Text style={styles.titulo}>Insira sua idade:</Text>
+          <TextInput style={styles.input} onChangeText={evt => this.setState({idade: evt})} label={'A'}/>
+        </View>
+        <View style={styles.label}>
+          <Text style={styles.titulo}>Selecione seu sexo</Text>
+          <Picker selectedValue={this.state.sexo} onValueChange={itemValue => {this.setState({sexo: itemValue})}}>
+            <Picker.Item value={1} key={1} label='Masculino'/>
+            <Picker.Item value={2} key={2} label='Feminino'/>
+          </Picker>
+        </View>
+        <View style={styles.label}>
+          <Text style={styles.titulo}>Selecione seu limite:</Text>
+          <Slider minimumValue={100} onValueChange={value => this.setState({limite: value})} maximumValue={10000} style={{flex: 1}}/>
+          <Text>{this.state.limite.toFixed()}</Text>
+        </View>
+        <View style={styles.label}>
+          <Text style={styles.titulo}>Estudante?</Text>
+          <Switch value={this.state.estudante} onValueChange={value => this.setState({estudante: value})}/>
+        </View>
+        <View style={styles.btnLabel}>
+          <TouchableOpacity onPress={() => this.enviar()} style={styles.button} value={'Criar conta'}><Text style={styles.buttonText}>Enviar</Text></TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -29,6 +69,39 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20
   },
+  label:{
+    flexDirection: 'row',
+    margin: 20
+  },
+  input:{
+    backgroundColor: 'gray',
+    marginLeft: 10,
+    flex: 1,
+    marginRight: 10,
+    borderColor: 'red',
+    borderRadius: 10,
+    color: 'black'
+  },
+  titulo:{
+    fontSize: 20,
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  },
+  button:{
+    text: 'red',
+    width: 100,
+    height: 40,
+    borderRadius:20,
+    backgroundColor: 'green'
+  },
+  buttonText:{
+    textAlign: 'center',
+    marginTop: 9
+  },
+  btnLabel:{
+    padding: 20,
+    alignItems: 'center'
+  }
 
 })
 
