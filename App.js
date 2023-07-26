2cc042d5443aa06ea70702938fa28922a82a8947
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-07-26 15:36:52 -0300

    Feat: Using Slider

--- App.js
+++ App.js
@@ -3,38 +3,30 @@ import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, FlatList }
 
 import {Picker} from '@react-native-picker/picker'
 import Pessoas from './src/Pessoas/'
-
+import Slider from '@react-native-community/slider'
 class App extends Component {
 
   constructor(props){
     super(props)
     this.state = {
-      selectedPizza: 0,
-      pizzas:[
-        {key: 1, nome: "Strogonoff", valor: 35.99},
-        {key: 2, nome: "Calabresa", valor: 33.99},
-        {key: 3, nome: "Quatro Queijos", valor: 50.99},
-        {key: 4, nome: "Mussarela", valor: 25.99},
-
-      ]
+      valor: 0
     }
   }
 
   render(){
 
-    let pizzasItem = this.state.pizzas.map( (v, k) => {
-      return <Picker.Item key={k} value={k} label={v.nome}/>
-    })
-
     return(
       <View style={styles.container}>
-        <Text style={styles.logo}>Menu de Pizzas</Text>
-        <Picker selectedValue={this.state.selectedPizza} onValueChange={ (itemValue, itemIndex) => this.setState({selectedPizza: itemValue})}>
-          {pizzasItem}
-        </Picker>
-
-        <Text style={styles.pizzas}>Você escolheu: {this.state.pizzas[this.state.selectedPizza].nome}</Text>
-        <Text style={styles.pizzas}>R$ {this.state.pizzas[this.state.selectedPizza].valor}</Text>
+        <Slider 
+          inimumValue={0}
+          maximumValue={100} 
+          onValueChange={ (valorSelecionado) => this.setState({valor: valorSelecionado}) }
+          value={this.state.valor}
+          minimumTrackTintColor={'green'}
+          maximumTrackTintColor={'red'}
+        />
+        <Text>{this.state.valor.toFixed()}</Text>
+        
       </View>
     );
   }
@@ -45,16 +37,6 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 20
   },
-  logo:{
-    textAlign: 'center',
-    fontSize: 28,
-    fontWeight: 'bold'
-  },
-  pizzas:{
-    marginTop: 15,
-    fontSize: 25,
-    textAlign: 'center'
-  }
 
 })
 
