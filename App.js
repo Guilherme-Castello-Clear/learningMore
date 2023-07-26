87b80c2fc0ce9cc97ed086243b3565060a4f6d9d
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-07-26 15:26:23 -0300

    Feat: Using Picker

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView, FlatList } from 'react-native'
 
+import {Picker} from '@react-native-picker/picker'
 import Pessoas from './src/Pessoas/'
 
 class App extends Component {
@@ -8,23 +9,32 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      feed:[
-        {id: '1', nome: 'Matheus', idade: 50, email: 'matheus@matheus.matheus'},
-        {id: '2', nome: 'Matheus', idade: 50, email: 'matheus@matheus.matheus'},
-        {id: '3', nome: 'Matheus', idade: 50, email: 'matheus@matheus.matheus'},
-        {id: '4', nome: 'Matheus', idade: 50, email: 'matheus@matheus.matheus'}
+      selectedPizza: 0,
+      pizzas:[
+        {key: 1, nome: "Strogonoff", valor: 35.99},
+        {key: 2, nome: "Calabresa", valor: 33.99},
+        {key: 3, nome: "Quatro Queijos", valor: 50.99},
+        {key: 4, nome: "Mussarela", valor: 25.99},
 
       ]
     }
   }
 
   render(){
+
+    let pizzasItem = this.state.pizzas.map( (v, k) => {
+      return <Picker.Item key={k} value={k} label={v.nome}/>
+    })
+
     return(
       <View style={styles.container}>
-        <FlatList
-          data={this.state.feed}
-          keyExtractor={(item) => item.id}
-          renderItem={ ({item}) => <Pessoas data={item}/>}/>
+        <Text style={styles.logo}>Menu de Pizzas</Text>
+        <Picker selectedValue={this.state.selectedPizza} onValueChange={ (itemValue, itemIndex) => this.setState({selectedPizza: itemValue})}>
+          {pizzasItem}
+        </Picker>
+
+        <Text style={styles.pizzas}>Você escolheu: {this.state.pizzas[this.state.selectedPizza].nome}</Text>
+        <Text style={styles.pizzas}>R$ {this.state.pizzas[this.state.selectedPizza].valor}</Text>
       </View>
     );
   }
@@ -32,8 +42,19 @@ class App extends Component {
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1
+    flex: 1,
+    marginTop: 20
   },
+  logo:{
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  pizzas:{
+    marginTop: 15,
+    fontSize: 25,
+    textAlign: 'center'
+  }
 
 })
 
